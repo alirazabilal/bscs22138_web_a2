bab6fdadec272fc6e09c8a5d71fa0e65e8d498df
alirazabilal <129405525+alirazabilal@users.noreply.github.com>
2024-10-05 14:12:57 +0500

    PART B,C,D,E OF Q2 ADDED CLIENT PART

---
+++ client2/hello/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileSender from "./ProfileSender";
+
+const App = () => {
+  return (
+    <div>
+      <ProfileSender />
+    </div>
+  );
+};
+
+export default App;
